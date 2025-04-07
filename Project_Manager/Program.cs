6fdb747e2b05b4e9c17626ebfb5cf863d95a28c0
Pham Van Dong <dong0705451521@gmail.com>
2025-04-07 17:11:50 +0700

    Start

---
+++ Project_Manager/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project_Manager.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//h
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+//h/
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+
+
+///////////////// cái này
+// Cấu hình API lắng nghe tất cả các IP
+builder.WebHost.UseUrls("http://0.0.0.0:5087"); // Lắng nghe trên tất cả các IP trong mạng và cổng 5087
+////////////
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseSwagger(); // thêm cái ni
+    app.UseSwaggerUI(); // thêm cái ni
+
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
